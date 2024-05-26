cfd87e62c6b536ebc2b6a6eab0b0bf7464b86797
Bruno Romero <brurrome@espol.edu.ec>
2024-05-26 01:43:35 -0500

    Colorear Hbox

--- TopMusical/TopMusical/src/main/java/espol/poo/topmusical/PrimaryController.java
+++ TopMusical/TopMusical/src/main/java/espol/poo/topmusical/PrimaryController.java
@@ -36,7 +36,7 @@ public class PrimaryController {
 
             HBox hb = new HBox(10);//hbox para ubicar info de cada cancion
             Label lbp = new Label(c.getPosActual() + "");
-            lbp.setStyle("-fx-font-weight: bold;-fx-font-size: 40;");
+            lbp.setStyle("-fx-font-weight: bold;-fx-font-size: 40;-fx-text-fill: white");
             ImageView iv = new ImageView();
             try {
                 Image img = new Image(new FileInputStream("img/" + c.getImagen()), 50, 50, true, true);
@@ -46,9 +46,10 @@ public class PrimaryController {
                 ex.printStackTrace();
             }
             Label lbT = new Label(c.getTitulo() + " \n" + c.getCantante());//titulo y cantante
-            lbT.setStyle("-fx-font-weight: bold;-fx-font-size: 15;");
+            lbT.setStyle("-fx-font-weight: bold;-fx-font-size: 14;-fx-text-fill: white");
 
             hb.getChildren().addAll(lbp, iv, lbT);//agregar al hbox
+            hb.setStyle("-fx-background-color: linear-gradient(to bottom, #690FCE, #12A1DA);");
 
             hb.setOnMouseClicked(eh -> mostrarHistorial(c));//establecer el evento del click
             vbTop10.getChildren().add(hb);//agregar al vbox
